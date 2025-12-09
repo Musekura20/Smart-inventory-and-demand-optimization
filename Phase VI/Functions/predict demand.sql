8d39521d042c8adf31955074fcccd5ace6616781
Musekura20 <inezaespe03@gmail.com>
2025-12-09 13:31:08 +0200

    Create predict demand.sql

---
+++ Phase VI/Functions/predict demand.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION predict_demand(
+  p_medicine_id IN NUMBER,
+  p_branch_id   IN NUMBER,
+  p_months      IN NUMBER := 3
+) RETURN NUMBER IS
+  v_avg NUMBER := 0;
+BEGIN
+  /*
+    Compute the simple monthly moving average of quantity sold for
+    the given medicine
+  */
+  SELECT NVL(ROUND(AVG(month_qty)), 0)
+    INTO v_avg
+    FROM (
+      SELECT TRUNC(t.Transaction_Date, 'MM') AS month_start,
+             SUM(ti.Quantity) AS month_qty
+      FROM Transactions t
+      JOIN Transaction_Items ti ON t.Transaction_ID = ti.Transaction_ID
+      WHERE ti.Medicine_ID = p_medicine_id
+        AND (p_branch_id IS NULL OR t.Branch_ID = p_branch_id)
+        AND t.Transaction_Date >= ADD_MONTHS(TRUNC(SYSDATE, 'MM'), -p_months)
+      GROUP BY TRUNC(t.Transaction_Date, 'MM')
+    );
+
+  RETURN v_avg;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    -- make sure you have a log_error(procedure_name, err_code, err_msg, backtrace) implementation
+    log_error_proc('predict_demand', 'OTHERS', SQLERRM, DBMS_UTILITY.FORMAT_ERROR_BACKTRACE);
+    RETURN 0;
+END predict_demand;
+/
